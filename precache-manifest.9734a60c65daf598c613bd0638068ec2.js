6e6d8c311e05ae3310b4aefa30019b5a064ba7fb
robotizm <razorson1313@gmail.com>
2020-07-25 23:13:56 +0300

    First contents

---
+++ precache-manifest.9734a60c65daf598c613bd0638068ec2.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dfe12a1c3199480c1d1",
+    "url": "/css/app.5ff2caf2.css"
+  },
+  {
+    "revision": "ae483ad340e0566cde56",
+    "url": "/css/chunk-vendors.8f11f3a4.css"
+  },
+  {
+    "revision": "3e3b0d4ebd3530bdd2fc9b36445f74ad",
+    "url": "/img/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cb9399bcd8e3341bbb9c1cf4760c8b8d",
+    "url": "/img/android-chrome-512x512.png"
+  },
+  {
+    "revision": "cc55236f315c5a861fd4a1d1c74c3931",
+    "url": "/img/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "a9e8eee24ad15115b78861b15373d0f5",
+    "url": "/img/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "38599fe2ec01e6d7d6e33111ba233309",
+    "url": "/img/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "658d44ade95336d1fc0423556b7c0a66",
+    "url": "/img/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "7d1c00d1ccfe31c81c4019bd228fa504",
+    "url": "/img/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "38599fe2ec01e6d7d6e33111ba233309",
+    "url": "/img/apple-touch-icon.png"
+  },
+  {
+    "revision": "6a3966fb7a834f454712c4596dd67d99",
+    "url": "/img/avatar.svg"
+  },
+  {
+    "revision": "a493ba0aa0b8ec8068d786d7248bb92c",
+    "url": "/img/browserconfig.xml"
+  },
+  {
+    "revision": "046f4ecdbca986ae860bd26eabca8bd5",
+    "url": "/img/favicon-16x16.png"
+  },
+  {
+    "revision": "6c98f81abf5b64650b9f80366822c31c",
+    "url": "/img/favicon-32x32.png"
+  },
+  {
+    "revision": "e001cdfeb6ff4a896752e40bfa8db313",
+    "url": "/img/mstile-150x150.png"
+  },
+  {
+    "revision": "2707eef7decceb9b09caed7683725369",
+    "url": "/img/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "b9aa277fcfc34c31db6c7a7ea3469b8c",
+    "url": "/img/site.webmanifest"
+  },
+  {
+    "revision": "0c386977c5c3988cb81aad9609c28fa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dfe12a1c3199480c1d1",
+    "url": "/js/app.3bd84a48.js"
+  },
+  {
+    "revision": "ae483ad340e0566cde56",
+    "url": "/js/chunk-vendors.8f891b51.js"
+  },
+  {
+    "revision": "b494ed0b0e221cd4a5a72e83e18d75ce",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
